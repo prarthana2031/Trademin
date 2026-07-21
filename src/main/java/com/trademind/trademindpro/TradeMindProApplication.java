28a038659f420a4721eebbd07d870a247ebce362
Preethi H J <preeee276@gmail.com>
2026-07-21 21:32:47 +0530

    week 1-P2

---
+++ src/main/java/com/trademind/trademindpro/TradeMindProApplication.java
@@ -0,0 +1,12 @@
+package com.trademind.trademindpro;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TradeMindProApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(TradeMindProApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/trademind/trademindpro/TradeMindProApplicationTests.java
@@ -0,0 +1,13 @@
+package com.trademind.trademindpro;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TradeMindProApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
