9820523a05bd103b1352703585d28ee2de63f63b
Nandini <nandinigow555@gmail.com>
2026-07-24 11:28:19 +0530

    Updated Implemented Fixed Window Rate Limiter

--- src/main/java/com/trademind/trademindpro/TradeMindProApplication.java
+++ src/main/java/com/trademind/trademindpro/TradeMindProApplication.java
@@ -6,8 +6,22 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class TradeMindProApplication {
+
     public static void main(String[] args) {
+
         SpringApplication.run(TradeMindProApplication.class, args);
+
+        RateLimiter rateLimiter = new RateLimiter();
+
+        for (int i = 1; i <= 6; i++) {
+
+            boolean allowed = rateLimiter.allowRequest("Nandini");
+
+            if (allowed) {
+                System.out.println("Request " + i + " : Allowed");
+            } else {
+                System.out.println("Request " + i + " : Rejected");
+            }
+        }
     }
 }
-    
